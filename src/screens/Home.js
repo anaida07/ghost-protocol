7c95c07b53294bcaaa1e5ab96dfece2258462876
Aneeta Sharma <get.aneeta@gmail.com>
2018-10-30 10:08:37 +0545

    Add task screen.

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -6,11 +6,11 @@ class Home extends React.Component {
     return (
       <div className="wrapper">
         <ul className="nav">
-          <Link to="#">Tasks</Link>
-          <Link to="#">Settings</Link>
-          <Link to="#">Edit Profile</Link>
-          <Link to="#">Notifications</Link>
-          <Link to="/">Logout</Link>
+          <Link to="/tasks" title="Tasks"><i className="fas fa-tasks"></i></Link>
+          <Link to="#" title="Settings"><i className="fas fa-cog"></i></Link>
+          <Link to="#" title="Edit Profile"><i className="fas fa-user"></i></Link>
+          <Link to="#" title="Notifications"><i className="fas fa-bell"></i></Link>
+          <Link to="/" title="Log Out"><i className="fas fa-sign-out-alt"></i></Link>
         </ul>
 
         <div className="main">
